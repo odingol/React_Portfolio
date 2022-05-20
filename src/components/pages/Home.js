5911504e390646fc0507cf8f0ce659da80bda6d7
Lamor Odingo <l2odingo@gmail.com>
2022-05-19 22:52:20 -0700

    linked view my work button

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,32 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 export default function Home() {
   const intro = {
     name: "Lamor Odingo",
     position: "I am a Front End Developer",
-    direction: "Below you will view the list of work I have completed on my journey to become a Web Developer and the best ways to reach out to me. Enjoy!"
-  }
+    direction:
+      "Below you will view the list of work I have completed on my journey to become a Web Developer and the best ways to reach out to me. Enjoy!",
+  };
 
   return (
-    <div name="Home" className="w-full h-screen flex justify-center items-center bg-indigo-600">
+    <div
+      name="Home"
+      className="w-full h-screen flex justify-center items-center bg-indigo-600"
+    >
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-white">Hello World! My name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-green-300">
           {intro.name}
         </h1>
-        <h2 className="text-4xl font-bold text-orange-300">
-          {intro.position}
-        </h2>
-        <p className="text-white py-4 max-w-[700px]">
-       {intro.direction}
-        </p>
+        <h2 className="text-4xl font-bold text-orange-300">{intro.position}</h2>
+        <p className="text-white py-4 max-w-[700px]">{intro.direction}</p>
         <div>
+          <Link to="/projects">
             <button className="animate-bounce hover:animate-none hover:bg-blue-300 bg-blue-200 px-10 py-3 text-xl uppercase mt-10 rounded-lg">
-              <Link to="/projects">View My Work</Link>
+              View My Work
             </button>
+          </Link>
         </div>
       </div>
     </div>
